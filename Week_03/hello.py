0d9a9106657fca8b12b54929b209e7552aecc019
김혜린 <pearlyn@naver.com>
2022-03-28 14:18:35 +0900

    week03_1

---
+++ Week_03/hello.py
@@ -0,0 +1,20 @@
+print("hello")
+
+import random
+print(random.random())
+
+print('''
+Hello
+World
+''')
+
+print('Hell World'.replace('Hell','Hello'))
+
+member=[1,2,3]
+
+print(random.choice(member))
+
+name='김혜린'
+
+print('안녕하세요'+name)
+print(f'안녕하세요 {name}')
